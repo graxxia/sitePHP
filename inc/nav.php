10ddf5b71d618b593dece428fe6a9689662d2316
gracia <graxxia@hotmail.com>
2020-02-23 23:21:41 -0500

    Added pages
    Added pages for conteny

---
+++ inc/nav.php
@@ -0,0 +1,9 @@
+<nav>
+<div class="nav-main">
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+</div>
+</nav>
--- index.php
+++ index.php
@@ -1,7 +1,24 @@
-<?php
-include('./inc/menu.php');
-include('./inc/header.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css"/>
+</head>
+<body>
+<nav>
+
+<div class="nav-main">
+    <div id="logo"> 
+    <img src="./img/roselogo-w.svg" alt="RoseLogo">
+    </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+</nav>
 
 <main>
 <div class="main-content">
@@ -11,6 +28,23 @@ include('./content/about.php');
 ?>
 </div>
 </main>
+
+<footer>
+    <div class="footer-main">
+        <p> Footer</p>
+    </div>
+</footer>
+    
+</body>
+</html>
+
+
+<?php
+include('./inc/menu.php');
+include('./inc/header.php');
+?>
+
+
 
 <?php
 
