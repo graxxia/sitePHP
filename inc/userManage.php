ecd8311ce65eafff14d2f2ef156d4f6ce95b3cb2
Theo <theocaz@gmail.com>
2020-02-26 12:26:51 -0500

    logout option in msg fixed

--- inc/userManage.php
+++ inc/userManage.php
@@ -1,7 +1,11 @@
 <div id="login-control">
 <?php
      if(isset($user)&& $user['status']){
-         echo "Logged in as: {$user['user']['username']}<a href=\"?logout\"></a>";
+         echo "Logged in as: {$user['user']['username']}"
+         ?>
+         <a href="?p=home&logout">logout</a>
+         <?php
+         
      }else{   
 ?>
         Not logget in <a href="../user/login.php">Login</a>
